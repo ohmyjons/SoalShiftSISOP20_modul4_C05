093fb59090606cbe7608900f05e6c0b1375734bc
iqbalhumam73 <58332363+iqbalhumam73@users.noreply.github.com>
2020-05-02 17:00:39 +0700

    Add files via upload

--- pp.c
+++ pp.c
@@ -13,16 +13,16 @@ char *log_path = "/home/iqbalhumam73/fs.log";
 char caesar[100] = "9(ku@AW1[Lmvgax6q`5Y2Ry?+sF!^HKQiBXCUSe&0M.b%rI'7d)o4~VfZ*{#:}ETt$3J-zpc]lnh8,GwP_ND|jO9(ku@AW1[L";
 
 void writeLog(char *level, char *cmd_desc){
-  FILE * fp;
-  fp = fopen (log_path, "a+");
-  time_t rawtime = time(NULL);
-  struct tm *info = localtime(&rawtime);
-  char time[100];
-  strftime(time, 100, "%y%m%d-%H:%M:%S", info);
-  char log[100];
-  sprintf(log, "%s::%s::%s\n", level, time, cmd_desc);
-  fputs(log, fp);
-  fclose(fp);
+    FILE * fp;
+    fp = fopen (log_path, "a+");
+    time_t rawtime = time(NULL);
+    struct tm *info = localtime(&rawtime);
+    char time[100];
+    strftime(time, 100, "%y%m%d-%H:%M:%S", info);
+    char log[100];
+    sprintf(log, "%s::%s::%s\n", level, time, cmd_desc);
+    fputs(log, fp);
+    fclose(fp);
 }
 
 void encryptV1(char *src) {
